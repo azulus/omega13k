cbf2ed7e8459a93950b0a53c908383d362641508
Kevin Grandon <kevingrandon@yahoo.com>
2016-08-15 18:08:04 -0700

    Add fitness for computer terminal.

---
+++ generators/fitness/computerterminal.js
@@ -0,0 +1,31 @@
+SHAPES.computerterminal = {}
+
+SHAPES.computerterminal.shapes = [_.RECTANGLE]
+
+SHAPES.computerterminal.fitness = (shapes) => {
+  if (shapes.length < 3) return false
+  try {
+    // Bail if any rect is outside of the main shape
+    let hasOutsideRect = null
+    for (let i = 0; i < shapes.length; i++) {
+      let compareToShape = shapes[i]
+      let compareToShapePts = compareToShape.pts
+      for (let j = 0; j < shapes.length; j++) {
+        let testShapePts = shapes[j].pts
+        if (testShapePts[0] > compareToShapePts[2] // right of compareTo
+         || testShapePts[2] < compareToShapePts[0] // left of compareTo
+         || testShapePts[1] > compareToShapePts[5] // above compareTo
+         || testShapePts[5] < compareToShapePts[1] // below compareTo
+        ) {
+          hasOutsideRect = shapes[j]
+        }
+      }
+    }
+    if (hasOutsideRect) return false;
+
+    return true;
+  } catch (e) {
+    console.log(e)
+    return false
+  }
+}
