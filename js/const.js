7f5fb4d9bd77c1025a620a363130cbbfaa90a72c
Jeremy Stanley <github@azulus.com>
2016-09-06 22:17:44 -0700

    sort of circles

--- js/const.js
+++ js/const.js
@@ -244,6 +244,8 @@ const FragmentShaderConst = {
   precision mediump float;
   varying vec3 vColor;
   void main(void) {
+    if(length(gl_PointCoord-vec2(0.5)) > 0.5)
+    discard;
     gl_FragColor = vec4(vColor, 1.);
     gl_FragColor.a = 1.;
   }
