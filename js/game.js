2fae9d15221ac7e82818f7121865f4267f6bf893
Jeremy Stanley <github@azulus.com>
2016-09-11 16:07:13 -0700

    Moving render functions to separate file

--- js/game.js
+++ js/game.js
@@ -33,62 +33,7 @@ $.assign($, {
 	_firstEnemyProjectileIdx: 0,
 	_firstPlayerProjectileIdx: 0,
 
-	// Game state
-	gameWon: false,
-
 	setTimeMultiplier: (tm) => {$.speedMultiplier = tm; return 1},
-
-	renderPlayer: (gl, prog, position) => {
-		$.drawShapesToCanvasGL(gl, prog, $.playerShapes, ...position)
-	},
-
-	renderEnemies: (gl, prog) => {
-		for (let i = 0; i < $._activeEnemyCount; i++){
-			let posIdx = i * 2;
-			let enemy = $.levelEnemies[$._activeEnemyIndexes[i]];
-			let shapes = enemy[LevelShipIndex.SHAPES];
-			let paths = enemy[LevelShipIndex.PATH_DATA];
-			// let pos = $.getPositionAtTime(paths, $.levelGameTime);
-
-			$.drawShapesToCanvasGL(gl, prog, shapes, $._activeEnemyPositions[posIdx], $._activeEnemyPositions[posIdx+1]);
-		}
-	},
-
-	renderEnemyProjectiles: (gl, prog, projectiles) => {
-		let count = 0;
-		$._activeEnemyProjectileCount = 0;
-		for (let i = $._firstEnemyProjectileIdx; i < projectiles.length; i++){
-			let [start, end, path] = projectiles[i];
-			if (start > $.levelGameTime) break;
-			if (end !== undefined) {
-				if ($._firstEnemyProjectileIdx === i) $._firstEnemyProjectileIdx++;
-				continue
-			};
-			let elapsedTime = $.levelGameTime - start;
-			let [x, y, xPerMs, yPerMs, startTime] = path;
-			let newX = x + (elapsedTime * xPerMs);
-			let newY = y + (elapsedTime * yPerMs)
-			if (newX < 0 || newY < 0 || newY > GameConst.HEIGHT) projectiles[i][1] = $.levelGameTime;
-			$._activeEnemyProjectilePositions[count++] = newX;
-			$._activeEnemyProjectilePositions[count++] = newY;
-			$._activeEnemyProjectileIndex[$._activeEnemyProjectileCount++] = i;
-		}
-
-		if (count > 0) {
-			let prog = $.prepareCanvasForProjectiles(gl, GameConst.WIDTH, GameConst.HEIGHT);
-
-			let pointsLoc = gl.getAttribLocation(prog, 'aPoint');
-			let colorLoc = gl.getUniformLocation(prog, 'u_color');
-			gl.uniform3f(colorLoc, 0.8, 0.4, 0.4);
-
-			gl.enableVertexAttribArray(pointsLoc);
-		  gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
-		  gl.bufferData(gl.ARRAY_BUFFER, $._activeEnemyProjectilePositions, gl.STATIC_DRAW);
-		  gl.vertexAttribPointer(pointsLoc, 2, gl.FLOAT, false, 0, 0);
-
-			gl.drawArrays(gl.POINTS, 0, count / 2);
-		}
-	},
 
 	checkEnemyProjectileCollisions: () => {
 		let count = 0;
@@ -115,52 +60,6 @@ $.assign($, {
 		}
 	},
 
-	renderPlayerProjectiles: (gl, prog, projectiles, pos) => {
-		let count = 0;
-		$._activePlayerProjectileCount = 0;
-		for (let i = $._firstPlayerProjectileIdx; i < projectiles.length; i++){
-			let [start, end, path, whatsThis] = projectiles[i];
-			if (start > $.levelGameTime) break;
-			if (end !== undefined) {
-				if ($._firstPlayerProjectileIdx === i) $._firstPlayerProjectileIdx++;
-				continue
-			};
-			let elapsedTime = $.levelGameTime - start;
-			let [x, y, xPerMs, yPerMs, startTime] = path;
-
-			// projectiles may be spawned off screen, recenter them based on pos
-			if (x === -1) {
-				[x, y] = pos;
-				x += GameConst.SHIP_WIDTH / 2;
-				y += GameConst.SHIP_HEIGHT / 2;
-				path[0] = x;
-				path[1] = y;
-			}
-
-			let newX = x + (elapsedTime * xPerMs);
-			let newY = y + (elapsedTime * yPerMs)
-			if (newX < 0 || newY < 0 || newY > GameConst.HEIGHT || newX > GameConst.WIDTH) projectiles[i][1] = $.levelGameTime;
-			$._activePlayerProjectilePositions[count++] = newX;
-			$._activePlayerProjectilePositions[count++] = newY;
-			$._activePlayerProjectileIndex[$._activePlayerProjectileCount++] = i;
-		}
-
-		if (count > 0) {
-			let prog = $.prepareCanvasForProjectiles(gl, GameConst.WIDTH, GameConst.HEIGHT);
-
-			let pointsLoc = gl.getAttribLocation(prog, 'aPoint');
-			let colorLoc = gl.getUniformLocation(prog, 'u_color');
-			gl.uniform3f(colorLoc, 0.6, 0.6, 1.);
-
-			gl.enableVertexAttribArray(pointsLoc);
-		  gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
-		  gl.bufferData(gl.ARRAY_BUFFER, $._activePlayerProjectilePositions, gl.STATIC_DRAW);
-		  gl.vertexAttribPointer(pointsLoc, 2, gl.FLOAT, false, 0, 0);
-
-			gl.drawArrays(gl.POINTS, 0, count / 2);
-		}
-	},
-
 	checkPlayerProjectileCollisions: () => {
 		let count = 0;
 		const projectiles = $.playerProjectiles;
@@ -182,70 +81,11 @@ $.assign($, {
 					)) {
 					// "Destroy" the projectile.
 					projectiles[i][1] = $.levelGameTime;
-
-					if ($.inBossLevel) {
-						// Currently special casing boss levels.
-						if ($.bossHealth > 0) $.bossHealth--;
-						else enemy[LevelShipIndex.KILL_TIME] = $.levelGameTime;
-					} else {
-						// Destroy the ship.
-						enemy[LevelShipIndex.KILL_TIME] = $.levelGameTime;
-					}
+					// Destroy the ship.
+					enemy[LevelShipIndex.KILL_TIME] = $.levelGameTime;
 				}
 			}
 		}
-	},
-
-	initializeBoss: (seed=1, idealProjectileWaves=3, idealProjectilesPerPath=8,
-			idealProjectilePaths=8, idealTimeBetweenProjectiles=1000,
-			projectileSpeed=100) => {
-
-		let r = $.getRandomNumberGenerator(seed),
-			waves = [],
-			path = $.generateBossPath(r),
-
-			// Fake end time for now for bosses.
-			endTime = 99999999,
-
-			bossWidth = GameConst.SHIP_WIDTH * 2,
-
-			bossHeight = GameConst.SHIP_HEIGHT * 2,
-
-			boss = $.getRandomFromArray(r, $.enemySpec),
-
-			bossR = $.getRandomNumberGenerator(boss[ObjectIndex.SEED]),
-
-			bossShapes = $.getRandomShapes(bossR, bossWidth, bossHeight, boss[ObjectIndex.SEED_SHAPE_STR]),
-
-			bossBoundingBox = $.getContainingBoundingBox(bossShapes),
-
-			timeBetweenProjectiles = $.floor($.randBetween(bossR, idealTimeBetweenProjectiles*.75, idealTimeBetweenProjectiles*1.25)),
-
-			// the projectile pattern to use
-			projectilePattern = $.generateProjectilePaths(
-				bossR,
-				ProjectilePathDirectionConst.LEFT,
-				0, 0, 0, idealProjectileWaves-1, idealProjectileWaves+1,
-				idealProjectilesPerPath-1, idealProjectilesPerPath+1,
-				idealProjectilePaths-1, idealProjectilePaths+1, 2000, projectileSpeed),
-
-			times = [];
-
-		// For bosses, push a single projectile path, and replay it.
-		for (var j = timeBetweenProjectiles; j < timeBetweenProjectiles + 10000; j += 200) {
-			let pos = $.getPositionAtTime(path, j);
-			let projectilePaths = $.offsetProjectilePaths(
-				projectilePattern,
-				pos[0] + bossWidth / 2,
-				pos[1] + bossHeight / 2,
-				j
-			).map(pp => [j, undefined, pp])
-			times.push([j, projectilePaths]);
-		}
-
-		waves.push([0, endTime, undefined, bossShapes, path, projectilePattern, times, 0, bossBoundingBox])
-
-		$.levelEnemies = waves;
 	},
 
 	initializeLevel: (seed=1, numWaves=10, idealMsBetweenWaves=5000,
@@ -445,18 +285,6 @@ $.assign($, {
 		}
 	},
 
-	renderHealth: (gl, prog, width, height) => {
-		let x = 0, y = 0, w = $.getCurrentPlayerHealth() / 100 * width, h = 6;
-		let shapes = [$.makeWebGLReady(['#afa',,[x,y,x+w,y,x+w,y+h,x,y+h]])];
-		$.drawShapesToCanvasGL(gl, prog, shapes, 0, 0);
-	},
-
-	renderChrono: (gl, prog, width, height) => {
-		let x = 0, y = 10, w = $.playerChrono / 1000 * width, h = 6;
-		let shapes = [$.makeWebGLReady(['#aaf',,[x,y,x+w,y,x+w,y+h,x,y+h]])];
-		$.drawShapesToCanvasGL(gl, prog, shapes, 0, 0);
-	},
-
 	prepareCanvasForProjectiles: (gl, width, height) => {
 	    let prog = $.getProjectilesProgram(gl)
 
@@ -605,29 +433,15 @@ let gameLoop = () => {
 		$.playerChrono += elapsedTime * PlayerConst.CHRONO_RECOVERY_PER_MS;
 	}
 
-	// Handle level change.
-	let wave = $.levelEnemies[$.levelEnemies.length - 1];
-	if (
-		// Normal wave
-		wave[LevelShipIndex.END_TIME] <= $.levelGameTime ||
-		$.inBossLevel && $._activeEnemyCount === 0) {
-		$.advanceLevel();
-	}
-
 	gl.flush();
 
 	if (shouldResumeNormal) {
 		$.setTimeMultiplier(1);
 	}
-
-	if ($.gameWon) {
-		console.log('game won.');
-	} else {
-		requestAnimationFrame(gameLoop);
-	}
 }
 
 $.initKeyboard();
 $.initializeGame()
-$.advanceLevel();
-requestAnimationFrame(gameLoop)
+$.initializeLevel();
+//requestAnimationFrame(gameLoop)
+setInterval(gameLoop, 16)
---
+++ js/render.js
@@ -0,0 +1,111 @@
+$.assign($, {
+	renderPlayer: (gl, prog, position) => {
+		$.drawShapesToCanvasGL(gl, prog, $.playerShapes, ...position)
+	},
+
+	renderEnemies: (gl, prog) => {
+		for (let i = 0; i < $._activeEnemyCount; i++){
+			let posIdx = i * 2;
+			let enemy = $.levelEnemies[$._activeEnemyIndexes[i]];
+			let shapes = enemy[LevelShipIndex.SHAPES];
+			let paths = enemy[LevelShipIndex.PATH_DATA];
+			// let pos = $.getPositionAtTime(paths, $.levelGameTime);
+
+			$.drawShapesToCanvasGL(gl, prog, shapes, $._activeEnemyPositions[posIdx], $._activeEnemyPositions[posIdx+1]);
+		}
+	},
+
+	renderEnemyProjectiles: (gl, prog, projectiles) => {
+		let count = 0;
+		$._activeEnemyProjectileCount = 0;
+		for (let i = $._firstEnemyProjectileIdx; i < projectiles.length; i++){
+			let [start, end, path] = projectiles[i];
+			if (start > $.levelGameTime) break;
+			if (end !== undefined) {
+				if ($._firstEnemyProjectileIdx === i) $._firstEnemyProjectileIdx++;
+				continue
+			};
+			let elapsedTime = $.levelGameTime - start;
+			let [x, y, xPerMs, yPerMs, startTime] = path;
+			let newX = x + (elapsedTime * xPerMs);
+			let newY = y + (elapsedTime * yPerMs)
+			if (newX < 0 || newY < 0 || newY > GameConst.HEIGHT) projectiles[i][1] = $.levelGameTime;
+			$._activeEnemyProjectilePositions[count++] = newX;
+			$._activeEnemyProjectilePositions[count++] = newY;
+			$._activeEnemyProjectileIndex[$._activeEnemyProjectileCount++] = i;
+		}
+
+		if (count > 0) {
+			let prog = $.prepareCanvasForProjectiles(gl, GameConst.WIDTH, GameConst.HEIGHT);
+
+			let pointsLoc = gl.getAttribLocation(prog, 'aPoint');
+			let colorLoc = gl.getUniformLocation(prog, 'u_color');
+			gl.uniform3f(colorLoc, 0.8, 0.4, 0.4);
+
+			gl.enableVertexAttribArray(pointsLoc);
+		  gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+		  gl.bufferData(gl.ARRAY_BUFFER, $._activeEnemyProjectilePositions, gl.STATIC_DRAW);
+		  gl.vertexAttribPointer(pointsLoc, 2, gl.FLOAT, false, 0, 0);
+
+			gl.drawArrays(gl.POINTS, 0, count / 2);
+		}
+	},
+
+	renderPlayerProjectiles: (gl, prog, projectiles, pos) => {
+		let count = 0;
+		$._activePlayerProjectileCount = 0;
+		for (let i = $._firstPlayerProjectileIdx; i < projectiles.length; i++){
+			let [start, end, path, whatsThis] = projectiles[i];
+			if (start > $.levelGameTime) break;
+			if (end !== undefined) {
+				if ($._firstPlayerProjectileIdx === i) $._firstPlayerProjectileIdx++;
+				continue
+			};
+			let elapsedTime = $.levelGameTime - start;
+			let [x, y, xPerMs, yPerMs, startTime] = path;
+
+			// projectiles may be spawned off screen, recenter them based on pos
+			if (x === -1) {
+				[x, y] = pos;
+				x += GameConst.SHIP_WIDTH / 2;
+				y += GameConst.SHIP_HEIGHT / 2;
+				path[0] = x;
+				path[1] = y;
+			}
+
+			let newX = x + (elapsedTime * xPerMs);
+			let newY = y + (elapsedTime * yPerMs)
+			if (newX < 0 || newY < 0 || newY > GameConst.HEIGHT || newX > GameConst.WIDTH) projectiles[i][1] = $.levelGameTime;
+			$._activePlayerProjectilePositions[count++] = newX;
+			$._activePlayerProjectilePositions[count++] = newY;
+			$._activePlayerProjectileIndex[$._activePlayerProjectileCount++] = i;
+		}
+
+		if (count > 0) {
+			let prog = $.prepareCanvasForProjectiles(gl, GameConst.WIDTH, GameConst.HEIGHT);
+
+			let pointsLoc = gl.getAttribLocation(prog, 'aPoint');
+			let colorLoc = gl.getUniformLocation(prog, 'u_color');
+			gl.uniform3f(colorLoc, 0.6, 0.6, 1.);
+
+			gl.enableVertexAttribArray(pointsLoc);
+		  gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
+		  gl.bufferData(gl.ARRAY_BUFFER, $._activePlayerProjectilePositions, gl.STATIC_DRAW);
+		  gl.vertexAttribPointer(pointsLoc, 2, gl.FLOAT, false, 0, 0);
+
+			gl.drawArrays(gl.POINTS, 0, count / 2);
+		}
+	},
+
+	renderHealth: (gl, prog, width, height) => {
+		let x = 0, y = 0, w = $.getCurrentPlayerHealth() / 100 * width, h = 6, i;
+		let shapes = [$.makeWebGLReady(['#afa',,[x,y,x+w,y,x+w,y+h,x,y+h]])];
+		$.drawShapesToCanvasGL(gl, prog, shapes, 0, 0);
+	},
+
+	renderChrono: (gl, prog, width, height) => {
+		let x = 0, y = 10, w = $.playerChrono / 1000 * width, h = 6;
+		let shapes = [$.makeWebGLReady(['#aaf',,[x,y,x+w,y,x+w,y+h,x,y+h]])];
+		$.drawShapesToCanvasGL(gl, prog, shapes, 0, 0);
+	}
+})
