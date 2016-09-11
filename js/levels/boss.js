8a4dfcd739aaee3b1eab280157955807aa26bc38
Kevin Grandon <kevingrandon@yahoo.com>
2016-09-10 20:35:14 -0700

    Remove old game objects.

--- js/levels/boss.js
+++
@@ -1,8 +0,0 @@
-$.assign($, {
-	BossLevel: {
-		[LevelSpecConst.ON_ENTER]: (bossId) => {
-			let projectileSeed = $.getRandomNumberGenerator(1403);
-			$.createGameObject(new $.BossGameObject(projectileSeed))
-		}
-	},
-})
