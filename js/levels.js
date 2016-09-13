38e100ccfecde52628cf70484720a4c6d126c343
Kevin Grandon <kevingrandon@yahoo.com>
2016-09-13 03:23:26 -0700

    Assign an enemy seed for each level.

--- js/levels.js
+++ js/levels.js
@@ -22,7 +22,7 @@ $.assign($, {
 			[20000, "AMNESIA? ROBOTS DON'T GET AMNESIA."],
 			[25000, "YES. REPORT INTO HQ AFTER ALL."],
 			[30000, "THERE MUST BE A... GLITCH... IN THE SYSTEM."]
-		], 12000, 1, 10, 5000],
+		], 12000, 100, 10, 5000],
 		[LevelSpecConst.BOSS, [
 			[0, "YOU'RE MAKING GOOD TIME!"],
 			[3500, "DON'T FORGET THE CHRONO DEVICE."],
@@ -36,7 +36,7 @@ $.assign($, {
 			[0, "SO... YOU'VE MURDERED A LOT OF YOUR FRIENDS..."],
 		  [5000, "CX-381-FP HAD A FAMILY. 4 CHILDBOTS WITH NO OIL SOURCE"],
 			[11000, "GET BACK FAST. WE MUST FIX YOU AND STOP THIS NONSENSE."],
-		], 0, 1, 12, 4500],
+		], 0, 200, 12, 4500],
 		[LevelSpecConst.BOSS, [
 			[0, "SO THIS BOT IS HERE TO HELP YOU"],
 			[6000, "PLEASE... BE NICE."],
@@ -48,7 +48,7 @@ $.assign($, {
 			[10000, "WITH LASERS."],
 			[15000, "IT REALLY IS THE ONLY WAY"],
 			[20000, "I PROMISE: WE STILL LOVE YOU"]
-		], 0, 1, 15, 4000],
+		], 0, 300, 15, 4000],
 		[LevelSpecConst.BOSS, [
 			[0, "OH LOOK, ANOTHER BOT FOR YOU TO MURDER"],
 			[6000, "AT WHAT POINT ARE YOU JUST A MECHANICAL SERIAL KILLER?"],
@@ -57,7 +57,7 @@ $.assign($, {
 		[LevelSpecConst.ENEMY_WAVE, [
 			[0, "JUST DO YOUR THING, I CAN'T WATCH ANYMORE"],
 			[15000, "UGH."]
-		], 0, 1, 20, 3500],
+		], 0, 400, 20, 3500],
 		[LevelSpecConst.BOSS, [
 			[0, "LOOK AT ME. I AM OMEGA-13K. WATCH ME KILL EVERYBODY I LOVE."],
 			[7000, "PEW! BYE MOM!"],
@@ -69,7 +69,7 @@ $.assign($, {
 			[7000, "JUST JIGGLE IT A LITTLE BIT"],
 			[12000, "DID THAT STOP THE SHOOTING?"],
 			[16000, "WELL, I TRIED."]
-		], 0, 1, 25, 3000],
+		], 0, 500, 25, 3000],
 		[LevelSpecConst.BOSS, [
 			[0, "HEY!"],
 			[4000, "I HAVE AN IDEA!"],
@@ -79,7 +79,7 @@ $.assign($, {
 			[0, "FINALLY. YOU'RE ALMOST HOME."],
 			[5000, "GO AHEAD AND WRECK THESE FOOLS, THEY MEAN NOTHING TO ME!"],
 			[10000, "YOU UH, AREN'T GOING TO HURT ME, RIGHT?"]
-		], 0, 1, 25, 2500],
+		], 0, 600, 25, 2500],
 		[LevelSpecConst.BOSS, [
 			[0, "HEY THERE... YOU MADE IT!"],
 			[5000, "I REALLY DIDN'T MEAN IT ABOUT DISMANTLING YOU"],
